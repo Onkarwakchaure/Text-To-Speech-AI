6c7109de0b767d2952a8cefd88bbe64a40e644a1
Onkar Wakchaure <onkarwakchaure59@outlook.com>
2026-06-07 21:03:58 +0530

    Voice cloning done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from TTS.api import TTS
+from pydub import AudioSegment
 
 import os
 import uuid
